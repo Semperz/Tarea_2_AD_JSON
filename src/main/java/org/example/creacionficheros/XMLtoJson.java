f5d793f9c84ca299332ad83c35d6652372329727
Sergio <serprigar@gmail.com>
2024-10-01 20:36:47 +0200

    feat(XMLtoJson.java): finish exercice 3 and 4

---
+++ src/main/java/org/example/creacionficheros/XMLtoJson.java
@@ -0,0 +1,34 @@
+package org.example.creacionficheros;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class XMLtoJson {
+    public static void main(String[] args) {
+        XmlMapper xmlMapper = new XmlMapper();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Como se llama el archivo?" + '\n');
+            StringBuilder path = new StringBuilder();
+            path.append("res/");
+            path.append(scanner.nextLine());
+            if (!Files.exists(Path.of(path.toString()))) {
+                System.out.println("El archivo no existe. Verifica la ruta.");
+                return;
+            }
+            JsonNode node = xmlMapper.readTree(new File(path.toString()));
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(path.toString().replace(".xml", ".json")),node);
+        }catch (IOException e){
+            System.out.println("No se ha encontrado el archivo, comprueba la ruta.");
+        }
+    }
+}
+
