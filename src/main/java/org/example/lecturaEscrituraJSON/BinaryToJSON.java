fbbe5c3eaceb6a96836b7a0588bb0c6a01c45117
a23sergiopg <serprigar@gmail.com>
2024-10-09 11:02:55 +0200

    refactor(BinaryToJSON.java): add game counter

--- src/main/java/org/example/lecturaEscrituraJSON/BinaryToJSON.java
+++ src/main/java/org/example/lecturaEscrituraJSON/BinaryToJSON.java
@@ -10,20 +10,23 @@ import java.util.List;
 public class BinaryToJSON {
     public static void main(String[] args) {
         List<Game> games = new ArrayList<>();
-
+        int numGames = 0;
         try (ObjectInputStream reader = new ObjectInputStream(new FileInputStream("res/games.bin"))){
             while(true){
                 Object o = reader.readObject();
                 if(o instanceof Game){
                     games.add((Game) o);
+                    System.out.println("Se ha leido un juego");
+                    numGames++;
                 }
             }
+
         }catch (EOFException e){
-            System.out.println("Se han leido los objetos");
+            System.out.println("Se terminó de leer el archivo, total de juegos: " + numGames);;
         }catch (IOException e) {
-            System.out.println("Error al leer el archivo.");
+            System.out.println("Error al leer el archivo."+ e.getMessage());
         }catch (ClassNotFoundException e){
-            System.out.println("No existe el archivo.");
+            System.out.println("No existe el archivo."+ e.getMessage());
         }
         ObjectMapper mapper = new ObjectMapper();
         try {
